3c00c9884dd5bd257ff0daadfa49c27bb2169485
wangflg <398332114@qq.com>
2018-03-06 22:04:42 +0800

    update bug

--- src/page/result/index.js
+++ src/page/result/index.js
@@ -12,10 +12,12 @@ var _mm = require('util/mm.js');
 // 通过接受的参数进行提示，$(function (){}整个页面加载完成后执行)
 $(function(){
     // 这里的type是页面中url中的值？type=register
-    var type        = _mm.getUrlParam('type') || 'default';
+    // 通过使用getUrlParam('')方法获取url地址中的type后面的值，如果没有就传default，显示操作成功
+    var type = _mm.getUrlParam('type') || 'default';
    console.log("type"+type)
     // 找到对应的元素进行渲染
-        $element    = $('.' + type + '-success');
+        // $element = $('.' + type + '-success');
+        var element=$('.' + type + '-success')
     // 显示对应的提示元素
-    $element.show();
+        element.show();
 })
